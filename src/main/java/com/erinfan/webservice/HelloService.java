c1980411f1aae25074f85b1834b6dc28a173f12c
techops <fanxiao1987821@gmail.com>
2014-05-27 20:42:27 +0800

    add service

---
+++ src/main/java/com/erinfan/webservice/HelloService.java
@@ -0,0 +1,5 @@
+package com.erinfan.webservice;
+
+public interface HelloService {
+    String sayHello(String name);
+}
---
+++ src/main/java/com/erinfan/webservice/HelloServiceImpl.java
@@ -0,0 +1,8 @@
+package com.erinfan.webservice;
+
+public class HelloServiceImpl implements HelloService{
+    @Override
+    public String sayHello(String name) {
+        return "Hello " + name + "!";
+    }
+}
